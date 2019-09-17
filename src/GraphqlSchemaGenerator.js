16cfd1ac7d81944195a754c199118be06f08c638
jeromecambon <jerome.cambon@bonitasoft.com>
2019-09-17 16:47:41 +0200

    Feat/enable new server (#5)
    * Code clean-up
    * Added 'port' parameter
    * Added checking on 'port' parameter

--- src/GraphqlSchemaGenerator.js
+++ src/GraphqlSchemaGenerator.js
@@ -1,3 +1,20 @@
+/*
+ * Copyright © 2019 Bonitasoft S.A.
+ * Bonitasoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 class GraphqlSchemaGenerator {
 
     constructor(bdmJson) {
@@ -62,8 +79,6 @@ class GraphqlSchemaGenerator {
 
         // Generate enums (?)
         // Add documentation for graphQL (?)
-
-        console.debug('schema=\n' + this.schema.join(''));
     }
 
     getResolvers() {
--- src/bdrServer.js
+++ src/bdrServer.js
@@ -1,14 +1,45 @@
+/*
+ * Copyright © 2019 Bonitasoft S.A.
+ * Bonitasoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 const {GraphQLServer} = require('graphql-yoga');
 const fs = require('fs');
 let xmlParser = require('xml-js');
 let GraphqlSchemaGenerator = require('./GraphqlSchemaGenerator');
 
+function getParameter(param) {
+    return param.substr(param.indexOf("=") + 1)
+}
+
 let bdmFile;
+let port = 4000;
 
 // Handle server parameters
 process.argv.forEach(function (val, index, array) {
     if (val.startsWith("bdmFile")) {
-        bdmFile = val.substr(val.indexOf("=") + 1);
+        bdmFile = getParameter(val);
+    }
+    if (val.startsWith("port")) {
+        let param = getParameter(val);
+        if (!isNaN(param)) {
+            port = param;
+        } else {
+            console.log("Invalid port: " + param);
+            process.exit(1);
+        }
     }
 });
 
@@ -18,24 +49,10 @@ if (bdmFile) {
     console.log("BDM added: " + bdmFile);
 }
 
-// Only one BDM handled for now
 let bdm = {
     description: 'Bonitasoft BDM',
     content: bdmContentXml
 };
-
-// const processUpload = async upload => {
-//     const {createReadStream, filename, mimetype, encoding} = await upload;
-//     const stream = createReadStream();
-//     const chunks = [];
-//     stream.on("data", function (chunk) {
-//         chunks.push(chunk);
-//     });
-//     stream.on("end", function () {
-//         bdm.content = Buffer.concat(chunks).toString();
-//         bdm.description = filename;
-//     });
-// };
 
 let bdmContentJson = xmlParser.xml2json(bdm.content, {compact: true, spaces: 4});
 
@@ -45,7 +62,6 @@ let schema = schemaGenerator.getSchema();
 
 const resolvers = {
     Query: {
-        // info: () => `Hello`,
     }
 };
 
@@ -55,4 +71,5 @@ const server = new GraphQLServer({
         resolvers}
     );
 
-server.start({endpoint: "/repository"}, () => console.log(`Server is running on http://localhost:4000`));
+server.start({port: port, endpoint: "/repository"}, () =>
+    console.log(`Server is running on http://localhost:${port}`));
