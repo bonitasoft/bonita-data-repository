ccdd79c5ec56750bcefc1221dcc6876bc8f4a829
benjaminparisel <benjamin.parisel@bonitasoft.com>
2020-05-19 17:51:22 +0200

    adding missing method

--- infrastructure/listDependencies.pipeline.groovy
+++ infrastructure/listDependencies.pipeline.groovy
@@ -64,3 +64,12 @@ ls -lRh .
         }
     }
 }
+
+def configGitCredentialHelper() {
+    sh """#!/bin/bash +x
+        set -e
+        echo "Using the git cache credential helper to be able to perform native git commands without passing authentication parameters"
+        # Timeout in seconds, ensure we have enough time to perform the whole process between the initial clone and the final branch push
+        git config --global credential.helper 'cache --timeout=18000'
+    """
+}
