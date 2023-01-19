ed3a2ae1a16ea16483aec22aa190321e56118e08
Romain Bioteau <romain.bioteau@bonitasoft.com>
2023-01-19 17:48:58 +0100

    chore(macos): add arm64 binary (#71)
    * build package for mac arm64 arch
    * sign win and macos binaries (required to run the arm64 binary)

--- Jenkinsfile
+++ Jenkinsfile
@@ -15,7 +15,7 @@ node {
     slackStage('🔧 Build', isBaseBranch) {
         def mvnArgs = 'verify'
         if (isBaseBranch) {
-            mvnArgs = "deploy -DaltDeploymentRepository=${env.ALT_DEPLOYMENT_REPOSITORY_SNAPSHOTS}"
+            mvnArgs = "deploy -Psign -DsignServiceURL=${env.SIGN_SERVICE_URL} -DmacSignServiceURL=${env.MAC_SIGN_SERVICE_URL} -DaltDeploymentRepository=${env.ALT_DEPLOYMENT_REPOSITORY_SNAPSHOTS}"
         }
         mvn "${mvnArgs}"
 
--- infrastructure/buildVersion.groovy
+++ infrastructure/buildVersion.groovy
@@ -7,7 +7,7 @@ pipeline {
         stage('Build and deploy') {
             steps {
                 configFileProvider([configFile(fileId: 'maven-settings', variable: 'MAVEN_SETTINGS')]) {
-                    sh("./mvnw -s ${MAVEN_SETTINGS} --no-transfer-progress -B deploy -DskipTests -DaltDeploymentRepository=${env.ALT_DEPLOYMENT_REPOSITORY_STAGING}")
+                    sh("./mvnw -s ${MAVEN_SETTINGS} --no-transfer-progress -B deploy -Psign -DsignServiceURL=${env.SIGN_SERVICE_URL} -DmacSignServiceURL=${env.MAC_SIGN_SERVICE_URL}  -DskipTests -DaltDeploymentRepository=${env.ALT_DEPLOYMENT_REPOSITORY_STAGING}")
                 }
             }
         }
