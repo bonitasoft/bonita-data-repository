1046d6defae4dc6cb23fcfa06a8f4fcbcae007f0
Romain Bioteau <romain.bioteau@bonitasoft.com>
2023-01-19 17:31:55 +0100

    sign all binaries

--- sign.sh
+++ sign.sh
@@ -3,19 +3,25 @@ set -e
 
 filename=$1
 url=$2
-workDir=$3
-zipFile="$filename.zip"
+output=$3
+workdir="$4"
 
-cd $workDir
-
-zip -q $zipFile -r $filename
-echo "Signing $zipFile..."
-HTTP_STATUS=$(curl --request POST -F exeFile=@$zipFile $url -w "%{http_code}" -o /tmp/$zipFile)
-if [ $HTTP_STATUS != 200 ]; then 
-	echo "An error occured while signing $zipFile..."
-	return 1
+echo "URL=$url"
+echo "Current folder = $PWD"
+cd $workdir
+echo "cd into $workdir"
+if [ -f $filename ]; then
+    echo "Signing $filename..."
+    HTTP_STATUS=$(curl --request POST -F exeFile=@$filename $url -w "%{http_code}" -o /tmp/$filename)
+    if [ $HTTP_STATUS != 200 ]; then 
+    	echo "An error occured while signing $filename..."
+    	return 1
+    fi
+    cat /tmp/$filename > $output
+    if [ $filename != $output ]; then
+        rm $filename
+        echo "rm $filename"
+    fi
+else
+   echo "$filename not found in $workdir. Codesign skipped."
 fi
-cat /tmp/$zipFile > $zipFile
-rm -r $filename
-unzip -q $zipFile
-rm $zipFile
