9f575031647078936ab0d4727e12a7eab31abaf1
Vincent HEMERY <vincent.hemery@bonitasoft.com>
2023-09-11 15:16:00 +0200

    tr(codesign): Move codesign to eSigner
    Use sign parameter for minimum signatures.

--- infrastructure/buildVersion.groovy
+++ infrastructure/buildVersion.groovy
@@ -7,7 +7,8 @@ pipeline {
         stage('Build and deploy') {
             steps {
                 configFileProvider([configFile(fileId: 'maven-settings', variable: 'MAVEN_SETTINGS')]) {
-                    sh("./mvnw -s ${MAVEN_SETTINGS} --no-transfer-progress -B deploy -Psign -DsignServiceURL=${env.SIGN_SERVICE_URL} -DmacSignServiceURL=${env.MAC_SIGN_SERVICE_URL}  -DskipTests -DaltDeploymentRepository=${env.ALT_DEPLOYMENT_REPOSITORY_STAGING}")
+                    def withSignProfile = params.sign ? '-Psign' : ''
+                    sh("./mvnw -s ${MAVEN_SETTINGS} --no-transfer-progress -B deploy ${withSignProfile} -DsignServiceURL=${env.SIGN_SERVICE_URL} -DmacSignServiceURL=${env.MAC_SIGN_SERVICE_URL}  -DskipTests -DaltDeploymentRepository=${env.ALT_DEPLOYMENT_REPOSITORY_STAGING}")
                 }
             }
         }
