1f33985a5ccfe083fbcc96463937fb2ae39449d5
Vincent HEMERY <vincent.hemery@bonitasoft.com>
2023-09-11 15:25:05 +0200

    Update buildVersion.groovy

--- infrastructure/buildVersion.groovy
+++ infrastructure/buildVersion.groovy
@@ -1,3 +1,4 @@
+def mavenProfiles = ''
 pipeline {
     agent any
     options {
@@ -7,8 +8,12 @@ pipeline {
         stage('Build and deploy') {
             steps {
                 configFileProvider([configFile(fileId: 'maven-settings', variable: 'MAVEN_SETTINGS')]) {
-                    def withSignProfile = params.sign ? '-Psign' : ''
-                    sh("./mvnw -s ${MAVEN_SETTINGS} --no-transfer-progress -B deploy ${withSignProfile} -DsignServiceURL=${env.SIGN_SERVICE_URL} -DmacSignServiceURL=${env.MAC_SIGN_SERVICE_URL}  -DskipTests -DaltDeploymentRepository=${env.ALT_DEPLOYMENT_REPOSITORY_STAGING}")
+                    script {
+                        if(params.sign) {
+                            mavenProfiles << '-Psign'
+                        }
+                    }
+                    sh("./mvnw -s ${MAVEN_SETTINGS} --no-transfer-progress -B deploy ${mavenProfiles} -DsignServiceURL=${env.SIGN_SERVICE_URL} -DmacSignServiceURL=${env.MAC_SIGN_SERVICE_URL}  -DskipTests -DaltDeploymentRepository=${env.ALT_DEPLOYMENT_REPOSITORY_STAGING}")
                 }
             }
         }
